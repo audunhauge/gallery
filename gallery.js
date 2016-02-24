b8a4fde744c71f58bf5b7c372836159f8d05386f
Audun Hauge <audun.hauge@gmail.com>
2016-02-24 14:04:40 +0100

    working on categories

--- gallery.js
+++ gallery.js
@@ -3,4 +3,10 @@ function setup() {
   var config = imagelist.mediagallery;
   var category = imagelist.mediagallery.category;
   var catsize = category.length; 
+  var catNames = [ ];
+  var i;
+  for (i = 0; i < catsize; i++) {
+    catNames.push(category[i].title);
+  }
+  document.getElementById("category").innerHTML = catNames.join('');
 }
